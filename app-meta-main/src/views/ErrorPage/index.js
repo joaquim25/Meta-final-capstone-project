7474c41e17a20484d9b4247a5b94d326fb839ea3
joaquim25 <quimze1996.25@gmail.com>
2023-04-14 11:51:32 +0100

    modified:   components/Navigation/styles.css 	modified:   index.css 	modified:   views/ErrorPage/index.js 	modified:   views/ErrorPage/styles.css

--- app-meta-main/src/views/ErrorPage/index.js
+++ app-meta-main/src/views/ErrorPage/index.js
@@ -9,15 +9,15 @@ const ErrorPage = ({ title = "", subtitle = "", children }) => {
   const error = useRouteError();
   return (
     <div id="main">
-        <div id="hello">
-          <h1 id="one">S</h1>
-          <h1 id="two">O</h1>
-          <h1 id="three">R</h1>
-          <h1 id="four">R</h1>
-          <h1 id="five">Y</h1>
-        </div>
-      <h2 id="con">WE ARE UNDER CONSTRUCTION</h2>
+      <div id="hello">
+        <h1 id="one">S</h1>
+        <h1 id="two">O</h1>
+        <h1 id="three">R</h1>
+        <h1 id="four">R</h1>
+        <h1 id="five">Y</h1>
       </div>
+    <h2 id="con">WE ARE UNDER CONSTRUCTION</h2>
+    </div>
   );
 };
 
